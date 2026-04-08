d75dac40e5d952a23e94112b1fdf33dc4626578b
chrizanet <chri.zanet@gmail.com>
2026-04-08 12:35:06 +0200

    Assign 1: First implementation of algebraic identity

--- 1_assignment/algebraic_identity/my_algebraic_identity.cpp
+++ 1_assignment/algebraic_identity/my_algebraic_identity.cpp
@@ -13,6 +13,82 @@ namespace {
 struct MyAlgebraicIdentity: PassInfoMixin<MyAlgebraicIdentity> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
 
+    // cycling on instructions
+    for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+      BasicBlock &B = *Iter;
+
+      for (auto I = B.begin(); I != B.end(); ++I) {
+        Instruction &Inst = *I;
+
+        // If it is a binary operation
+        BinaryOperator *BinOp = dyn_cast<BinaryOperator>(&Inst);
+        if (!BinOp) continue;
+        Value *Op0 = BinOp->getOperand(0);
+        Value *Op1 = BinOp->getOperand(1);
+        
+        // ADD CASE
+        if (BinOp->getOpcode() == Instruction::Add){
+          ConstantInt *C = dyn_cast<ConstantInt>(Op0);
+          Value *Other = Op1;
+
+          if (!C) {
+            C = dyn_cast<ConstantInt>(Op1);
+            Other = Op0;
+          }
+
+          // If there are no constants, continue
+          if (!C) continue;
+          
+          // C->getValue().isZero() is the same as C->getValue() == 0
+          if (C->getValue() == 0){
+            BinOp->replaceAllUsesWith(Other);
+          }
+        }
+
+        // SUB CASE
+        if (BinOp->getOpcode() == Instruction::Sub){
+          // first operator is not a constant, second operator is a constant
+          if (!dyn_cast<ConstantInt>(Op0) && dyn_cast<ConstantInt>(Op1)){
+            Value *First = Op0;
+            ConstantInt *Second = dyn_cast<ConstantInt>(Op1);
+            if (Second->getValue().isZero()){
+              BinOp->replaceAllUsesWith(First);
+            }
+          }
+        }
+
+        // MUL CASE
+        if (BinOp->getOpcode() == Instruction::Mul){
+          ConstantInt *C = dyn_cast<ConstantInt>(Op0);
+          Value *Other = Op1;
+
+          if (!C) {
+            C = dyn_cast<ConstantInt>(Op1);
+            Other = Op0;
+          }
+
+          // If there are no constants, continue
+          if (!C) continue;
+          
+          if (C->getValue().isOne()){
+            BinOp->replaceAllUsesWith(Other);
+          }
+        }
+
+        // DIV CASE
+        if (BinOp->getOpcode() == Instruction::Div){
+          // first operator is not a constant, second operator is a constant
+          if (!dyn_cast<ConstantInt>(Op0) && dyn_cast<ConstantInt>(Op1)){
+            Value *First = Op0;
+            ConstantInt *Second = dyn_cast<ConstantInt>(Op1);
+            if (Second->getValue().isOne()){
+              BinOp->replaceAllUsesWith(First);
+            }
+          }
+        }
+
+      }
+    }
     return PreservedAnalyses::all();
   }
   static bool isRequired() { return true; }
