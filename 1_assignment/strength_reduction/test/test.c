391ceee611b659b09f51769eb67a11314ad542a4
chrizanet <chri.zanet@gmail.com>
2026-04-17 22:58:35 +0200

    Assignment 1: strength reduction test updates

--- 1_assignment/strength_reduction/test/test.c
+++ 1_assignment/strength_reduction/test/test.c
@@ -1,13 +1,13 @@
-// Test per Strength Reduction Pass
+// Test for Strength Reduction Pass
 
 int test_mul(int a) {
     int x1, x2, x3, x4, x5;
 
-    x1 = a * 8;      // Power of 2 (2^3) --> a << 3
-    x2 = a * 9;      // Power of 2 + 1 (2^3 + 1) --> (a << 3) + a
-    x3 = a * 7;      // Power of 2 - 1 (2^3 - 1) --> (a << 3) - a
-    x4 = 15 * a;     // Power of 2 - 1 (2^4 - 1) - commutativity test --> (a << 4) - a
-    x5 = a * 6;      // Not a power of 2 or adjacent --> no opt
+    x1 = a * 8;      // power of 2 (2^3) --> a << 3
+    x2 = a * 9;      // power of 2 + 1 (2^3 + 1) --> (a << 3) + a
+    x3 = a * 7;      // power of 2 - 1 (2^3 - 1) --> (a << 3) - a
+    x4 = 15 * a;     // power of 2 - 1 (2^4 - 1) - commutativity test --> (a << 4) - a
+    x5 = a * 6;      // not a power of 2 or adjacent --> no opt
 
     return x1 + x2 + x3 + x4 + x5;
 }
@@ -15,8 +15,8 @@ int test_mul(int a) {
 unsigned int test_udiv(unsigned int a) {
     unsigned int x1, x2;
 
-    x1 = a / 4;      // Power of 2 (2^2) --> a >> 2 (lshr)
-    x2 = a / 7;      // Not a power of 2 --> no opt
+    x1 = a / 4;      // power of 2 (2^2) --> a >> 2 (lshr)
+    x2 = a / 7;      // not a power of 2 --> no opt
 
     return x1 + x2;
 }
@@ -24,8 +24,8 @@ unsigned int test_udiv(unsigned int a) {
 int test_sdiv(int a) {
     int x1, x2;
 
-    x1 = a / 16;     // Power of 2 (2^4) --> (a + (a < 0 ? 15 : 0)) >> 4 (ashr)
-    x2 = a / 3;      // Not a power of 2 --> no opt
+    x1 = a / 16;     // power of 2 (2^4) --> (a + (a < 0 ? 15 : 0)) >> 4 (ashr)
+    x2 = a / 3;      // not a power of 2 --> no opt
 
     return x1 + x2;
 }
@@ -33,10 +33,9 @@ int test_sdiv(int a) {
 int test_limit_cases(int a) {
     int x1, x2, x3;
 
-    x1 = a * -8;     // Negative constant (sgt(0) check) --> no opt
-    x2 = a / -4;     // Negative divisor (sgt(0) check) --> no opt
-    x3 = a * 1;      // Constant is 1 (isPowerOf2 is true, log is 0) --> a << 0 
-                     // Nota: tecnicamente x << 0 e' identita', gestita gia' da Algebraic Identity
+    x1 = a * -8;     // negative constant (sgt(0) check) --> no opt
+    x2 = a / -4;     // negative divisor (sgt(0) check) --> no opt
+    x3 = a * 1;      // constant is 1 (isPowerOf2 is true, log is 0) --> a << 0 
 
     return x1 + x2 + x3;
 }
