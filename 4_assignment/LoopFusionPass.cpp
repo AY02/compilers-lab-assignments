81352d217a7e1f294a59640b3889f3c59cd5cdad
chrizanet <chri.zanet@gmail.com>
2026-05-27 13:23:08 +0200

    Clarifications

--- 4_assignment/LoopFusionPass.cpp
+++ 4_assignment/LoopFusionPass.cpp
@@ -26,12 +26,14 @@ struct LoopFusionPass: PassInfoMixin<LoopFusionPass> {
     // loops are guarded:
     // they are adjacent if the guard of the first points to the guard
     // of the second, and the guard of the second only contains that
-    // control statement. 
+    // control statement. Also the preheaer of the second loop needs
+    // to contain only the branch 
     if (L0->isGuarded() && L1->isGuarded()){
 
     }
 
     // loops are not guarded
+    // temporary note: still to be checked
     else {
       if (TLL_0->getExitBlock() == TLL_1->getLoopPreheader() && TLL->getExitBlock() != nullptr){
         if (TLL_1->getLoopPreheader()->size() == 1){
@@ -59,6 +61,7 @@ struct LoopFusionPass: PassInfoMixin<LoopFusionPass> {
     LoopInfo &LI = FAM.getResult<LoopAnalysis>(F);
     DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
 
+    // THIS IS WHAT I MADE IN CLASS: (TO BE REMOVED)
     // 1) adjacent loops
     bool adjacent = false;
 
@@ -71,9 +74,6 @@ struct LoopFusionPass: PassInfoMixin<LoopFusionPass> {
       // if there is a successive loop
       if (next_it != LI.end()){
         Loop *TLL_1 = *next_it;
-
-        // posso controllare se entrambi sono guarded: nel caso in cui abbiano la stessa condizione
-        // chiaramente o eseguono insieme o non esegue nessuno
 
         // loop 0 is guarded
         if (TLL_0->isGuarded()){
@@ -88,8 +88,6 @@ struct LoopFusionPass: PassInfoMixin<LoopFusionPass> {
 
           if (Instr_Guard->getSuccessor(0) == TLL_1_Entry || Instr_Guard->getSuccessor(1) == TLL_1_Entry){
             if (TLL_1_Entry != nullptr){
-              // sono adiacenti se la guardia del primo punta alla guardia del secondo
-              // e il secondo contiene solo quello statement di controllo
               adjacent = true;
             }
           }
