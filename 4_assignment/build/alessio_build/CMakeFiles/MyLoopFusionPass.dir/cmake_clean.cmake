496ce8eeb42f0d74d74554c3bff60e5a58fba081
AY02 <alessioyang.02@gmail.com>
2026-06-16 10:10:08 +0200

    Build

---
+++ 4_assignment/build/alessio_build/CMakeFiles/MyLoopFusionPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyLoopFusionPass.dir/home/ayang/Documenti/unimore/compilatori/parte2/compilers-lab-assignments/4_assignment/my_loop_fusion.cpp.o"
+  "CMakeFiles/MyLoopFusionPass.dir/home/ayang/Documenti/unimore/compilatori/parte2/compilers-lab-assignments/4_assignment/my_loop_fusion.cpp.o.d"
+  "libMyLoopFusionPass.pdb"
+  "libMyLoopFusionPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyLoopFusionPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
